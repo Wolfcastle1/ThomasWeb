172b352bcc6384bfefbaadad781404659d4b7924
Samuel Thomas <54728781+Wolfcastle1@users.noreply.github.com>
2021-02-26 20:30:44 -0600

    Home Component and Grid Outlines

--- thomas-web-front/src/App.js
+++ thomas-web-front/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+
+//chess icon
+import { GiChessKnight } from 'react-icons/gi'
+
+import Home from './pages/home.js'
+
+// Routing Elements
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    // <div className="App">
+    <div>
+      <header className="myApp-header">
+        <h3>Thomas Web</h3>
+        <GiChessKnight />
+
+
       </header>
+
+      <div className="content-back">
+        <Router>
+
+          {/* buttons for navigation */}
+
+          <Switch>
+            <Route exact path='/' component={Home}></Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ thomas-web-front/src/pages/home.js
@@ -0,0 +1,28 @@
+import React from 'react'; 
+
+function Home() {
+    return (
+        <div className="home-grid">
+            <div className='bubble-div'> 
+            <p>hello</p>
+            </div>
+            
+            <div className='bubble-div'> 
+            <p>There</p>
+            </div>
+
+            <div className='bubble-div'> 
+            <p>General</p>
+            </div>
+
+            <div className='bubble-div'> 
+            <p>Kenobi</p>
+            </div>
+
+            
+            
+        </div>
+    );
+}
+
+export default Home; 
